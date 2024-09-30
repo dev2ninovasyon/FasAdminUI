3c4b31e97ff3f1cf7b06243b631c529281c11525
2NMaster <2NMaster@DESKTOP-KL01N7V>
2024-09-30 12:47:04 +0300

    Genel Hesap Plani

---
+++ src/api/GenelHesapPlani/GenelHesapPlani.ts
@@ -0,0 +1,65 @@
+import { url } from "../apiBase";
+
+export const getDosyaBilgileri = async (token: string) => {
+  try {
+    const response = await fetch(`${url}`, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.error("Dosya Bilgileri getirilemedi");
+    }
+  } catch (error) {
+    console.error("Bir hata oluştu:", error);
+  }
+};
+
+export const getGenelHesapPlani = async (token: string, tip: string) => {
+  try {
+    const response = await fetch(`${url}/Mizan/GenelHesapPlani?tip=${tip}`, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.ok) {
+      return response.json();
+    } else {
+      console.error("Dosya Bilgileri getirilemedi");
+    }
+  } catch (error) {
+    console.error("Bir hata oluştu:", error);
+  }
+};
+
+export const deleteDosyaBilgileri = async (token: string, selected: any) => {
+  try {
+    const response = await fetch(
+      `${url}
+        `,
+      {
+        method: "DELETE",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(selected),
+      }
+    );
+
+    if (response.ok) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error("Bir hata oluştu:", error);
+  }
+};
